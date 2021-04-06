62967edfc3347e077ffc516b62ceda596a5f2c94
nh-g <giangngohong@gmail.com>
2021-04-06 21:24:50 +0200

    format parcellist

--- src/pages/ParcelList.jsx
+++ src/pages/ParcelList.jsx
@@ -6,36 +6,36 @@ import ParcelItem from "../components/parcels/ParcelItem";
 import { parcelState } from "../state/parcelState";
 
 export default function ParcelList() {
-const [parcels, setParcels] = useRecoilState(parcelState);
-const [load, setLoad] = useState(false);
-const [error, setError] = useState("");
+  const [parcels, setParcels] = useRecoilState(parcelState);
+  const [load, setLoad] = useState(false);
+  const [error, setError] = useState("");
 
-const ParcelsArray = parcels.map((item) => (
-<ParcelItem key={item.id} parcel={item} />
-));
+  const ParcelsArray = parcels.map((item) => (
+    <ParcelItem key={item.id} parcel={item} />
+  ));
 
-let count = 0;
-useEffect(() => {
-console.log("useEffect", count++);
-FetchData("https://my.api.mockaroo.com/orders.json?key=e49e6840")
-.then((res) => {
-setParcels(res);
-setLoad(true);
-})
-.catch((err) => {
-setError(err);
-setLoad(true);
-});
-}, []);
+  let count = 0;
+  useEffect(() => {
+    console.log("useEffect", count++);
+    FetchData("https://my.api.mockaroo.com/orders.json?key=e49e6840")
+      .then((res) => {
+        setParcels(res);
+        setLoad(true);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoad(true);
+      });
+  }, []);
 
-if (load) {
-return (
-  <ul className="parcel-grid">
-    <h1>Your Parcels</h1>
-    {error ? <li>{error.message}</li> : ParcelsArray}
-  </ul>
-);
-} else {
-return <div>Loading...</div>;
-}
+  if (load) {
+    return (
+      <ul className="parcel-grid">
+        <h1> Your Parcels </h1>{" "}
+        {error ? <li> {error.message} </li> : ParcelsArray}
+      </ul>
+    );
+  } else {
+    return <div> Loading... </div>;
+  }
 }
