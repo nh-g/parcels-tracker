9f7796ffa4a8fbc11b7a14814097092a55da5192
nh-g <giangngohong@gmail.com>
2021-04-06 21:25:52 +0200

    comment out unused import

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,9 @@
 // NPM packages
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faSearch} from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {faSearch} from "@fortawesome/free-solid-svg-icons";
+// import { Link } from "react-router-dom";
 
 export default function SearchBar() {
   // Local state
